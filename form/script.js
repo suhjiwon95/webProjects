ca078e44408d0f0ecadd5e35ebaa994377cb117c
Jiwon Suh <suhjiwon1995@gmail.com>
2023-07-12 22:26:55 -0400

    grid practice

--- form/script.js
+++ form/script.js
@@ -1,24 +1,42 @@
 const submit = document.getElementById('submit');
-const form = document.getElementById('form');
+const elements = document.getElementById('form').elements;
+const form = document.getElementById('form')
 const fname = document.getElementById('fname');
+const error = document.querySelector("input + span.error");
 const requestsDiv = document.querySelector('.list'); //parent node of newList
 const nameArr = []; //store names
 const newList = document.createElement('li'); //create li element for displaying names
+const input = document.querySelector('input');
 
-const logSubmit = (e) => {
-    alert(`${fname.value}'s form was submitted`);
-    e.preventDefault();
-    // window.open("./confirmation.html");
-
-    nameArr.push(`${fname.value}`); //append name to nameArr
-    localStorage.setItem("nameLists", nameArr); //store nameArr in a web browser
-    const nameLists = localStorage.getItem("nameLists"); //get nameArr
-
-    newList.innerText = nameLists; //updates the name on screen. Display all the names stored in the nameArr
-    requestsDiv.appendChild(newList); //append p element to parent node
-    console.log(nameArr);
-
+const validateForm = (event) => {
+    for(let i = 0; i < elements.length; i++) {
+        if (elements[i].type === "radio") {
+            if (elements[i].checked) {
+                console.log(elements[i].value);
+            }
+        } else {
+            if (elements[i].value === '') {
+                error.textContent = 'Fill out this field';
+                console.log('nope');
+                event.preventDefault();
+    
+            } else {    
+                console.log(elements[i].value);
+                nameArr.push(`${elements[i].value}`); //append name to nameArr
+                // localStorage.setItem("nameLists", nameArr); //store nameArr in a web browser
+                // const nameLists = localStorage.getItem("nameLists"); //get nameArr
+                // newList.innerText = nameLists; //updates the name on screen. Display all the names stored in the nameArr
+                // requestsDiv.appendChild(newList); //append p element to parent node
+                console.log(nameArr);
+               
+            }
+        }
+        
+    }
+    
 
 }
 
-form.addEventListener("submit", logSubmit);
+form.addEventListener('submit', validateForm);
+
+
